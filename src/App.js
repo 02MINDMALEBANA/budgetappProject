21b2b4d364455c929d6086241f1b5731b75eefb6
02MINDMALEBANA <malebanakgopo@gmail.com>
2022-06-20 10:47:11 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState} from 'react';
 // import AddItem from './components/addItem';
 // import DisplayTransaction from './components/displayTransaction';
 import Home from './components/home';
-import {BrowserRouter as Router , Switch , Route} from 'react-router-dom'
+import {BrowserRouter as Router , Switch , Route} from 'react-router-dom';
 import Login from './components/login';
 import SignUp from './components/signup';
+// import BrowserRouter, {BrowserRouter as Router , Switch , Route} from 'react-router-dom/BrowserRouter';
+
+// ReactDOM.render((
+//   <BrowserRouter basename={process.env.PUBLIC_URL}>
+//     <App />
+//   </BrowserRouter>
+// ), ...)
+
 
 function App() {
 
   const [transaction, setTransaction] = useState ([]);
   useEffect( () => {
+    console.log('the app fails');
     console.log(transaction);
   }, [transaction])
 
@@ -32,7 +41,7 @@ function App() {
   })
   return (
 
-    <Router>
+    <Router basename='02MINDMALEBANA/budgetappProject'>
       <Switch>
         <Route exact path="/" component={Login}></Route>
         <Route path="/sign-up" component={SignUp}></Route>
